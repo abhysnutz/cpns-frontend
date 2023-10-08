ab7e6566f554992b4d199d7daedf3ad554a9f4ae
Mohamad Syafri Lamato <maps.abhy25@gmail.com>
2023-10-08 15:02:39 +0800

    update auth flow

--- src/pages/app/Home.jsx
+++ src/pages/app/Home.jsx
@@ -2,10 +2,31 @@ import React, { useEffect } from 'react'
 import TokenVerify from '../../utils/TokenVerify'
 
 export default function Home() {
-  // useEffect(() => {
-  //     TokenVerify()
-  // })
-  return (
-    <div>Home</div>
-  )
+    
+    
+    useEffect(() => {
+        TokenVerify()
+    },[])
+
+    // const TokenVerify = async () => {
+    //     try {
+    //         const token = localStorage.getItem('token');
+    //         const signature = token.split('.')[2]
+    //         const decoded = jwt_decode(token);
+            
+    //         const response = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/session-verify`,{userId:decoded.userId, token:signature});
+            
+    //         if(!response.data.session){
+    //             localStorage.removeItem('token')
+    //             location.reload()
+    //         }
+    //     } catch (error) {
+    //         localStorage.removeItem('token')
+    //         location.reload()
+    //     }
+    // }
+
+    return (
+        <div>Home</div>
+    )
 }
--- src/utils/TokenVerify.jsx
+++ src/utils/TokenVerify.jsx
@@ -1,20 +1,36 @@
 import axios from 'axios';
 import React from 'react'
 import { Navigate } from 'react-router-dom';
+import jwt_decode from 'jwt-decode'
 
 export default async function TokenVerify() {
     const token = localStorage.getItem('token');
 
     if(!token) return <Navigate to="/auth/login" />;
 
-    await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/verify`,{token})
-        .then((response) => {
-            if(response.data.success == false){
-                localStorage.removeItem('token');
-                window.location.reload();
-            }
-        })
-        .catch((error) => {
-            console.log(error.message);
-        })
+    try {
+        const signature = token.split('.')[2]
+        const decoded = jwt_decode(token);
+
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/session-verify`,{userId:decoded.userId, token:signature});
+        
+        console.log(response.data);
+        if(!response.data.session){
+            localStorage.removeItem('token')
+            location.reload()
+        }
+    } catch (error) {
+        localStorage.removeItem('token')
+        location.reload()
+    }
+    // await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/verify`,{token})
+    //     .then((response) => {
+    //         if(response.data.success == false){
+    //             localStorage.removeItem('token');
+    //             window.location.reload();
+    //         }
+    //     })
+    //     .catch((error) => {
+    //         console.log(error.message);
+    //     })
 }
